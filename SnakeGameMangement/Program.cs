f8f1970e6d6d63e66ea7842a802f33e91901ec87
Anandmohanty <mohantyanand3@gmail.com>
2023-01-11 01:40:22 +0530

    [Anand] - Player One

---
+++ SnakeGameMangement/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakeGameMangement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake & Ladder Simulator");
+
+            int position = 0;
+
+            Console.WriteLine("Welcome Player 1");
+            Console.WriteLine("Player 1 is at position : " + position);
+
+        }
+    }
+}
